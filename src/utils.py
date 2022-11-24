ca9db3e3482f8ccc97fa198b0305adea12e184f9
lhydave <lhydave@163.com>
2022-11-24 11:43:31 +0800

    add predict_error in utils for double check.

--- src/utils.py
+++ src/utils.py
@@ -5,12 +5,12 @@ from typing import TypedDict
 
 RetType = TypedDict(
     'RetType', {'acc_rate': float, 'correct_num': int, 'total_num': int})
-DEFAULT_OUTPUT_PATH = '../output/'
+DEFAULT_OUTPUT_DIR = '../output/'
 
 
 # calculate prediction error from a file
-def predict_error(data_filename: str, predict_name='pred', label_name='label') -> RetType:
-    data = pd.read_json(os.path.join(DEFAULT_OUTPUT_PATH, data_filename))
+def predict_error(data_filename: str, output_dir: str = DEFAULT_OUTPUT_DIR, predict_name: str = 'pred', label_name: str = 'label') -> RetType:
+    data = pd.read_json(os.path.join(output_dir, data_filename))
     correct_num = len(data[data[predict_name] == data[label_name]])
     total_num = len(data)
     acc_rate = correct_num / total_num
