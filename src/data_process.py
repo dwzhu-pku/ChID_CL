ee2f018ecb713b08b022b44ac461d38db86a98ca
lhydave <lhydave@163.com>
2022-12-07 23:00:21 +0800

    data analysis finsihed.

--- src/data_process.py
+++ src/data_process.py
@@ -14,7 +14,7 @@ OUTPUT_PATH = '../dataset/'
 dict1_path = os.path.join(DEFAULT_RAW_PATH, 'chinese-idioms-12976.txt')
 dict2_path = os.path.join(DEFAULT_RAW_PATH, 'xinhua-idiom.json')
 
-universe_set_path = os.path.join(OUTPUT_PATH, 'train_data_10w.json')
+universe_set_path = os.path.join(OUTPUT_PATH, 'train_data.json')
 
 output1_path = os.path.join(OUTPUT_PATH, 'idiom-dict1.json')
 output2_path = os.path.join(OUTPUT_PATH, 'idiom-dict2.json')
@@ -26,7 +26,7 @@ realCount_tag = 'realCount'
 candidate_num = 7
 
 
-def produce_samples(dict_df: pd.DataFrame, output_path: str):
+def produce_samples(dict_df: pd.DataFrame, output_path: str) -> list[dict]:
     print(
         f'producing samples from {dict_df.shape[0]} idioms, saving to {output_path}')
     # get all the idioms
@@ -56,32 +56,32 @@ def produce_samples(dict_df: pd.DataFrame, output_path: str):
         sample = {}
         sample[groundTruth_tag] = [idiom]
 
-        # from meaning to get the candidates
-        if not pd.isnull(dict_df['explanation'][i]) and len(dict_df['explanation'][i]) > 1:
-            cands = np.random.choice(
-                idioms[idioms != idiom], candidate_num-1, replace=False).tolist()
-            cands.append(idiom)
-            np.random.shuffle(cands)
-            sample[candidates_tag] = [cands]
-
-            content = f'#idiom#：{dict_df["explanation"][i]}'
-            sample[content_tag] = content
-            sample[realCount_tag] = 1
-            samples_from_dict.append(copy.deepcopy(sample))
-
-        # # from example to get the candidates
-        # if not pd.isnull(dict_df['example'][i]):
+        # # from meaning to get the candidates
+        # if not pd.isnull(dict_df['explanation'][i]) and len(dict_df['explanation'][i]) > 1:
         #     cands = np.random.choice(
         #         idioms[idioms != idiom], candidate_num-1, replace=False).tolist()
         #     cands.append(idiom)
         #     np.random.shuffle(cands)
         #     sample[candidates_tag] = [cands]
 
-        #     content = dict_df['example'][i].replace('～', '#idiom#')
-        #     if '#idiom#' in content:
-        #         sample[content_tag] = content
-        #         sample[realCount_tag] = 1
-        #         samples_from_dict.append(copy.deepcopy(sample))
+        #     content = f'#idiom#：{dict_df["explanation"][i]}'
+        #     sample[content_tag] = content
+        #     sample[realCount_tag] = 1
+        #     samples_from_dict.append(copy.deepcopy(sample))
+
+        # from example to get the candidates
+        if not pd.isnull(dict_df['example'][i]):
+            cands = np.random.choice(
+                idioms[idioms != idiom], candidate_num-1, replace=False).tolist()
+            cands.append(idiom)
+            np.random.shuffle(cands)
+            sample[candidates_tag] = [cands]
+
+            content = dict_df['example'][i].replace('～', '#idiom#')
+            if '#idiom#' in content:
+                sample[content_tag] = content
+                sample[realCount_tag] = 1
+                samples_from_dict.append(copy.deepcopy(sample))
 
         # # from derivation to get the candidates
         # if not pd.isnull(dict_df['derivation'][i]):
@@ -98,21 +98,32 @@ def produce_samples(dict_df: pd.DataFrame, output_path: str):
         #         samples_from_dict.append(copy.deepcopy(sample))
 
     # save the samples
-    with open(output_path, 'w', encoding='utf-8') as f:
-        for sample in samples_from_dict:
-            f.write(json.dumps(sample, ensure_ascii=False) + '\n')
+    # with open(output_path, 'w', encoding='utf-8') as f:
+    #     for sample in samples_from_dict:
+    #         f.write(json.dumps(sample, ensure_ascii=False) + '\n')
 
-    print('samples produced and saved!')
-
+    # print('samples produced and saved!')
+    return samples_from_dict
 
 # process the data from dict1
 dict1 = pd.read_csv(dict1_path, sep=',', header=None)
 dict1.columns = ['id', 'word', 'pinyin',
                  'explanation', 'derivation', 'example', 'abbreviation']
 # print(dict1.head())
-produce_samples(dict1, output1_path)
+sample1 = produce_samples(dict1, output1_path)
 
 # process the data from dict2
 dict2 = pd.read_json(dict2_path, orient='records', encoding='utf-8')
 # print(dict2.head())
-produce_samples(dict2, output2_path)
+sample2 = produce_samples(dict2, output2_path)
+
+# merge the samples, delete the duplicates about groundTruth
+samples = sample1 + sample2
+groundTruths = [sample[groundTruth_tag][0] for sample in samples]
+groundTruths, indices = np.unique(groundTruths, return_index=True)
+samples = [samples[i] for i in indices]
+# save the samples
+with open(os.path.join(OUTPUT_PATH, 'idiom-dict-example.json'), 'w', encoding='utf-8') as f:
+    for sample in samples:
+        f.write(json.dumps(sample, ensure_ascii=False) + '\n')
+
